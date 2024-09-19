853881a85398a4d6c51a1f88355f5887044558a8
md5502 <m.baniasadi.d@gmail.com>
2024-09-19 21:24:04 +0330

    add the postgres database config

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "urlshorter",
+        "USER": "user",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     },
 }
 
