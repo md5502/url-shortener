f0b419d43cfd53212af6307a8cd0fbac108f7ea5
md5502 <m.baniasadi.d@gmail.com>
2024-09-22 10:37:50 +0330

    fix some bugs

--- api/models.py
+++ api/models.py
@@ -5,13 +5,15 @@ from django.db import models
 class Url(models.Model):
     full_url = models.URLField(db_index=True)
     short_url = models.CharField(max_length=20, db_index=True, blank=True, unique=True)
-    counter = models.IntegerField()
+    counter = models.IntegerField(default=0)  # Initialize counter to 0
 
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.full_url
 
-    def save(self, *args, **kwargs) -> None:
-        self.short_url = short_url.encode_url(self.id)
-        return super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.short_url:  # Only generate short_url if it doesn't exist
+            super().save(*args, **kwargs)  # Save first to generate the ID
+            self.short_url = short_url.encode_url(self.id)
+        super().save(*args, **kwargs)
