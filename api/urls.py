1aa568d2eb0a6ac2ebc9672a8922c6de52af83d8
md5502 <m.baniasadi.d@gmail.com>
2024-11-25 18:47:29 +0330

    change the views and urls for the Url model

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,15 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.urls import path
 
-from .views import UrlView
+from .views import create_url, delete_url, go_to_base_url, list_url, retrieve_url, update_url
 
 app_name = "api"
 
-
 urlpatterns = [
-    path("<short_url>/", UrlView.as_view(), name="UrlView"),
-    *debug_toolbar_urls(),
+    path("list", list_url),
+    path("create", create_url),
+    path("update/<str:short_code>", update_url),
+    path("delete/<str:short_code>", delete_url),
+    path("<str:short_code>", retrieve_url),
+    path("go/<str:short_code>", go_to_base_url),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,28 +1,96 @@
 from django.shortcuts import get_object_or_404, redirect
-from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 
 from .models import Url
-from .serializers import UrlSerializer
+from .serializers import UrlSerializerIn, UrlSerializerOut
+from .utils import delete_cache, get_cached_url, set_cached_url, update_cache
 
 
-class UrlView(APIView):
-    def get(self, request, short_url=None):
-        if short_url:
-            url = get_object_or_404(Url, short_url=short_url)
-            f_url = url.full_url
-            url.counter += 1
-            url.save()
-            return redirect(f_url)
-        # List all URLs if no short_url is provided
-        urls = Url.objects.all()
-        serializer = UrlSerializer(urls, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(["get"])
+def list_url(request):
+    data = Url.objects.all()
 
-    def post(self, request):
-        serializer = UrlSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()  # Save the validated data to the database
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    data = UrlSerializerOut(data, many=True).data
+
+    return Response(data)
+
+
+
+@api_view(["POST"])
+def create_url(request):
+    url_data = request.data.get("url")
+    if not url_data:
+        return Response({"msg": "URL field is required."}, status=HTTP_400_BAD_REQUEST)
+
+    serializer = UrlSerializerIn(data={"url": url_data})
+    if serializer.is_valid():
+        obj = Url.objects.create(url=url_data)
+        # Save URL to cache
+        set_cached_url(obj.short_code, obj.url, obj.access_count)
+
+        response_data = UrlSerializerOut(obj).data
+        return Response(response_data, status=HTTP_201_CREATED)
+
+    return Response(
+        {"msg": "Validation error", "errors": serializer.errors},
+        status=HTTP_400_BAD_REQUEST,
+    )
+
+
+@api_view(["PUT"])
+def update_url(request, short_code):
+    obj = get_object_or_404(Url, short_code=short_code)
+
+    url = request.data.get("url")
+    if not url :
+        return Response({"msg": "URL field is required."}, status=HTTP_400_BAD_REQUEST)
+
+    serializer = UrlSerializerIn(data={"url": url})
+    if serializer.is_valid():
+        obj.url = serializer.data.get("url")
+        update_cache(short_code, obj.url)
+        obj.save()
+
+        response_data = UrlSerializerOut(obj).data
+        return Response(response_data, status=HTTP_200_OK)
+
+    return Response(
+        {
+            "msg": "Validation error",
+            "errors": serializer.errors,
+        },
+        status=HTTP_400_BAD_REQUEST,
+    )
+
+@api_view(["DELETE"])
+def delete_url(request, short_code):
+    obj = get_object_or_404(Url, short_code=short_code)
+    obj.delete()
+    delete_cache(short_code)
+    return Response(
+        {
+            "msg": f"url with the {short_code} deleted successfully",
+        },
+        status=HTTP_204_NO_CONTENT,
+    )
+
+
+
+@api_view(["GET"])
+def retrieve_url(request, short_code):
+    url = get_object_or_404(Url, short_code=short_code)
+    serializer = UrlSerializerOut(url)
+    return Response(data=serializer.data, status=HTTP_200_OK)
+
+
+
+@api_view(["GET"])
+def go_to_base_url(request, short_code):
+    url = get_cached_url(short_code)
+    if url:
+        return redirect(url)
+    obj = get_object_or_404(Url, short_code=short_code)
+    set_cached_url(obj.short_code, obj.url, obj.access_count)
+    return redirect(obj.url)
