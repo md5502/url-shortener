9e7a9f39079c292cbc7f82f9e29f0db2adc34d37
md5502 <m.baniasadi.d@gmail.com>
2024-11-25 18:48:03 +0330

    add the api documentation urls and the api app url

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("", include("api.urls")),
     path("admin/", admin.site.urls),
-]
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+
+    path("api/shorter/", include("api.urls")),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
